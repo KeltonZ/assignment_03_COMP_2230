84813be51721a03c59de3e008b24f6dd54d0bf93
Kelton Zinn <kzinn2@academic.rrc.ca>
2025-10-01 21:41:50 -0500

    Update: Wrote a function that will have the scenario result passed to it, and display the end screen this may be inefficient but it made sense in my mind when writing

--- adventure-game/script.js
+++ adventure-game/script.js
@@ -26,7 +26,7 @@ const story = [
 			or we can fall back to the city, but potentially put them at risk?`,
 		choices: [
 			{choiceOne: "retreat to the city?", next: winScreen1},
-			{choiceTwo; "Split your forces?", next: loseScreen1}
+			{choiceTwo: "Split your forces?", next: loseScreen1}
 		]
 	},
 
@@ -51,10 +51,51 @@ const story = [
 			or risk being caught attempting to regroup?`,
 		choices: [
 			{choiceOne: "Counter Strike?", next: loseScreen3}
-			{choiceTwo: "Rally and Regroup?", next: winScreen3}
+			{choiceTwo: "Rally and Regroup?", next: winScreen2}
 		]
 	}
 ];
+
+// will be called to display end screens win or lose.
+function endings(result) {
+
+	// Win Screens
+	try {
+		if (result === "winScreen1") {
+			winScreen1.textContent = `Victory - You retreat to the city and 
+				by repositioning to regain the advantage you successfully 
+				hold off the attack.`
+		
+		} else if (result === "winScreen2") {
+			winScreen2.textContent = `Victory - Though risky you recognize that 
+				we need every man we can get, we regroup successfully and hold 
+				off the attackers breaking the siege. Well done!`
+		
+		// Lose Screens
+		} else if (result === "loseScreen1") {
+			loseScreen1.textContent = `Defeat - By splitting your forces neither force is capable 
+				of successfully holding back the enemy. You lose many in a pyric victory.`
+		
+		} else if (result === "loseScreen2") {
+			loseScreen2.textContent = `Defeat -   One of the oldest maneuvers 
+			in the book, the pincer, due to being surrounded the battle 
+			is too heavily to their advantage and we end up being overrun.`
+			
+		} else if (result === "loseScreen3") {
+			loseScreen3.textContent = `Defeat - We end up not having enough forces 
+			to effectively stall the advance, we lose too much to 
+			attrition and are overrun.`
+		}
+
+		else {
+			throw new Error("Argument passed does not match any results" {result});
+		}
+	} catch (error) {
+		console.log(error.textContent)
+	} finally {
+		// call the render function here once written kelton
+	}
+}
     
 
 
