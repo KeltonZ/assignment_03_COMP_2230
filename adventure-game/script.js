ea00e240b0df24608890e92db7da70b4565cc789
Kelton Zinn <kzinn2@academic.rrc.ca>
2025-10-01 22:27:59 -0500

    Update: Created the nextQuestion function to properly call the renderQuestion function. Since I didnt notice the call  in the HTML the first time the renderQuestion function has some of the functionality that nextQuestion would have had I made it with this in mind.

--- adventure-game/script.js
+++ adventure-game/script.js
@@ -1,57 +1,61 @@
 console.log("Game Initialized, ")
 
-let currentState = intro
+const scenario = document.getElementById("question")
+const answersDiv = document.getElementById("answers")
+const nextBtn = document.getElementById("next-btn")
+
+let currentState = "intro"
 
 const story = [
 
     //Intro Scenario
   {  
-    id: intro,
+    id: "intro",
     scenario: `You're the general of an army 
     	tasked with setting up the defense of your city during a siege.
     	You receive information from scouts that a force will be flanking your position, 
     	but the reliability of the information is dubious at best.`, 
 		choices: [
-			{choiceOne: "Trust the scout?", next: lineOneFallBack},
-			{choiceTwo: "Ignore the scout?", next: lineTwoFlanked}
+			{text: "Trust the scout?", next: "lineOneFallBack"},
+			{text: "Ignore the scout?", next: "lineTwoFlanked"}
 		]
 	},
 	
 	//Line one: Decision one
 	{
-		id: lineOneFallBack,
+		id: "lineOneFallBack",
 		scenario: `We are currently positioned outside of the city, 
 			do we re-allocate a part of our forces to defend against the flank,
 			but risk compromising our strength as a whole, 
 			or we can fall back to the city, but potentially put them at risk?`,
 		choices: [
-			{choiceOne: "retreat to the city?", next: winScreen1},
-			{choiceTwo: "Split your forces?", next: loseScreen1}
+			{text: "retreat to the city?", next: "winScreen1"},
+			{text: "Split your forces?", next: "loseScreen1"}
 		]
 	},
 
 	//Line Two: Decision one
 	{
-		id: lineTwoFlanked,
+		id: "lineTwoFlanked",
 		scenario: `The scouts were correct, and now our forces are in a 
 			horrible position. They're almost upon us, do we fall back to 
 			the city and risk collateral or reposition to counter the flank?`,
 		choices: [
-			{choiceOne: "Fall back?", next: lineThreeFallBack},
-			{choiceTwo: "Reposition?", next: loseScreen2}
+			{text: "Fall back?", next: "lineThreeFallBack"},
+			{text: "Reposition?", next: "loseScreen2"}
 		]
 	},
 
 	//Line Three: Decision one
 	{
-		id: lineThreeFallBack,
+		id: "lineThreeFallBack",
 		scenario: `We fall back and barely manage to escape.
     	Unfortunately we are scattered and lost some forces, do we initiate 
 			a counter strike to stall and destabilize their advance 
 			or risk being caught attempting to regroup?`,
 		choices: [
-			{choiceOne: "Counter Strike?", next: loseScreen3}
-			{choiceTwo: "Rally and Regroup?", next: winScreen2}
+			{text: "Counter Strike?", next: "loseScreen3"},
+			{text: "Rally and Regroup?", next: "winScreen2"}
 		]
 	}
 ];
@@ -59,23 +63,25 @@ const story = [
 // will be called to display end screens win or lose.
 function endings(result) {
 
+	answersDiv.innerHTML = '';
+  nextBtn.style.display = 'inline-block';
 	// Win Screens
 	try {
 		if (result === "winScreen1") {
 			winScreen1.textContent = `Victory - You retreat to the city and 
-				by repositioning to regain the advantage you successfully 
-				hold off the attack.`
-		
+			by repositioning to regain the advantage you successfully 
+			hold off the attack.`
+			
 		} else if (result === "winScreen2") {
 			winScreen2.textContent = `Victory - Though risky you recognize that 
-				we need every man we can get, we regroup successfully and hold 
-				off the attackers breaking the siege. Well done!`
-		
-		// Lose Screens
+			we need every man we can get, we regroup successfully and hold 
+			off the attackers breaking the siege. Well done!`
+			
+			// Lose Screens
 		} else if (result === "loseScreen1") {
 			loseScreen1.textContent = `Defeat - By splitting your forces neither force is capable 
-				of successfully holding back the enemy. You lose many in a pyric victory.`
-		
+			of successfully holding back the enemy. You lose many in a pyric victory.`
+			
 		} else if (result === "loseScreen2") {
 			loseScreen2.textContent = `Defeat -   One of the oldest maneuvers 
 			in the book, the pincer, due to being surrounded the battle 
@@ -86,17 +92,55 @@ function endings(result) {
 			to effectively stall the advance, we lose too much to 
 			attrition and are overrun.`
 		}
-
+		
 		else {
-			throw new Error("Argument passed does not match any results" {result});
+			throw new Error(`Argument passed does not match any results ${result}`);
 		}
 	} catch (error) {
 		console.log(error.textContent)
 	} finally {
 		// call the render function here once written kelton
 	}
-}
-    
+};
 
+// Render currentState
+
+function nextQuestion() {
+	nextBtn.style.display = "none";
+	renderQuestion();
+};
+
+
+function renderQuestion() {
+		const storyNode = story.find(n => n.id === currentState);	
+
+		if (!storyNode) {
+			console.log(`Not a story id, passing to endings: ${currentState}`)
+			endings(currentState)
+			return;
+		}
+
+		// sets the question to the current scenario
+		scenario.textContent = storyNode.scenario;
+		
+		//clears the choice buttons
+		answersDiv.innerHTML = '';
+
+		storyNode.choices.forEach(choice => {
+			const li = document.createElement('li');
+			const btn = document.createElement('button');
+
+			btn.textContent = choice.text;
+			btn.onclick = () => {
+				currentState = choice.next;
+				renderQuestion();
+			};
+
+			li.appendChild(btn);
+			answersDiv.appendChild(li);
+		});
+
+		//nextBtn.style.display = 'none' when css is created I'll need to disable the button
+}
 
 
